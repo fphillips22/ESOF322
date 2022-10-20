c4716e3fab2a278c679671c52303f84ff6db8577
fphillips22 <fphillips1209@gmail.com>
2022-10-19 21:25:11 -0600

    changed database into mySQL from sqlite

--- djangoProject/database/system_2/settings.py
+++ djangoProject/database/system_2/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+ATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
